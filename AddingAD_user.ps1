9d6aa822e84eb3bacad2ccd80f2a69f7eb30cf91
takvam <alexander8921@hotmail.no>
2024-08-27 14:39:27 +0200

    Adding AD user

--- AddingAD_user.ps1
+++ AddingAD_user.ps1
@@ -1,5 +1,28 @@
 ## Adding AD users
 
+## Create a function to generate a random pw
+function New-RandomPassword {
+    param(
+        [Parameter()]
+        [int]$MinimumPasswordLength = 5,
+        [Parameter()]
+        [int]$MaximumPasswordLength = 10,
+        [Parameter()]
+        [int]$NumberOfAlphaNumericCharacters = 5,
+        [Parameter()]
+        [switch]$ConvertToSecureString
+    )
+    
+    Add-Type -AssemblyName 'System.Web'
+    $length = Get-Random -Minimum $MinimumPasswordLength -Maximum $MaximumPasswordLength
+    $password = [System.Web.Security.Membership]::GeneratePassword($length,$NumberOfAlphaNumericCharacters)
+    if ($ConvertToSecureString.IsPresent) {
+        ConvertTo-SecureString -String $password -AsPlainText -Force
+    } else {
+        $password
+    }
+}
+
 $customernumber = Read-Host "Enter the customernumber for the customer"
 ##Filtering on the customernumber-name as provided above
 $OrganisationNumber = Get-ADOrganizationalUnit -Filter "Name -like '$customerNumber*'" 
@@ -24,26 +47,3 @@ catch [Microsoft.ActiveDirectory.Management.ADIdentityResolutionException] {
 }
 
 New-ADUser -DisplayName $username -UserPrincipalName $username -Surname $last -GivenName $first -EmailAddress $email -Name "$first $last" -Instance $users Get-ADPrincipalGroupMembership $users[0]
-
-## Create a function to generate a random pw
-function New-RandomPassword {
-    param(
-        [Parameter()]
-        [int]$MinimumPasswordLength = 5,
-        [Parameter()]
-        [int]$MaximumPasswordLength = 10,
-        [Parameter()]
-        [int]$NumberOfAlphaNumericCharacters = 5,
-        [Parameter()]
-        [switch]$ConvertToSecureString
-    )
-    
-    Add-Type -AssemblyName 'System.Web'
-    $length = Get-Random -Minimum $MinimumPasswordLength -Maximum $MaximumPasswordLength
-    $password = [System.Web.Security.Membership]::GeneratePassword($length,$NumberOfAlphaNumericCharacters)
-    if ($ConvertToSecureString.IsPresent) {
-        ConvertTo-SecureString -String $password -AsPlainText -Force
-    } else {
-        $password
-    }
-}
